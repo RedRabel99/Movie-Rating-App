09d92d65e6c6ea16d191f9bf7b15cb90959d59fb
Pawel Lysko <lyskopawel03@gmail.com>
2023-07-03 22:34:12 +0200

    feature: setup frontend react app
    add homepage that lists all available movies

--- src/commonMain/kotlin/BaseResponse.kt
+++ src/commonMain/kotlin/BaseResponse.kt
@@ -1,26 +1,23 @@
-package me.user.application.utils
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import io.ktor.http.*
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonSerialize
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonSerialize
+@Serializable
 sealed class BaseResponse<T>(
-    @JsonIgnore open val statusCode: HttpStatusCode
+    @Contextual open val statusCode: HttpStatusCode
 ) {
-    @JsonSerialize
     data class SuccessResponse<T>(
         val data: T? = null,
         val message: String? = null,
-        @JsonIgnore
+        @Contextual
         override val statusCode: HttpStatusCode = HttpStatusCode.OK
     ): BaseResponse<T>(statusCode)
-    @JsonSerialize
     data class ErrorResponse(
         val message: String,
-        @JsonIgnore
+        @Contextual
         override val statusCode: HttpStatusCode = HttpStatusCode.BadRequest
     ) : BaseResponse<Any>(statusCode)
 }
